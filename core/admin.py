af7805ecaba9cf38a65f67b983a8bd938dad8952
dejvinczi <dejvinczi.main@gmail.com>
2024-04-03 20:02:21 +0200

    Created and registered in django admin Artist model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Artist
+
+class ArtistModelAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Artist, ArtistModelAdmin)
--- core/models.py
+++ core/models.py
@@ -9,3 +9,16 @@ class Event(models.Model):
     name = models.CharField(max_length=150, unique=True)
     start = models.DateTimeField()
     end = models.DateTimeField()
+
+    
+class Artist(models.Model):
+    """Artist representation in db."""
+    MUSIC_GENRE_CHOICES = [
+        ('rock', 'Rock'),
+        ('pop', 'Pop'),
+        ('hip_hop', 'Hip Hop'),
+        ('country', 'Country'),
+    ]
+    
+    name = models.CharField(unique=True)
+    music_genre = models.CharField(max_length=7, choices=MUSIC_GENRE_CHOICES)
